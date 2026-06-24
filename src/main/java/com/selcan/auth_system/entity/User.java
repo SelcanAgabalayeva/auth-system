03429b5ce048972dc9cdea7d68dea75c62dbd544
SelcanAgabalayeva <selcanagabalayeva12@gmail.com>
2026-06-25 00:40:47 +0400

    Initial auth system setup

---
+++ src/main/java/com/selcan/auth_system/entity/User.java
@@ -0,0 +1,52 @@
+package com.selcan.auth_system.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false,length = 20)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Builder.Default
+    private Boolean isActive = true;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/selcan/auth_system/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.selcan.auth_system.repositories;
+
+import com.selcan.auth_system.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+}
